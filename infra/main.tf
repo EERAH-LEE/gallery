5849230b3814ae2a224b8f8b1a901f2231cb5124
cloud-sue <sue2019522@gmail.com>
2026-05-21 11:37:57 +0900

    root main.tf 수정

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source    = "./infra/modules/network"
+  source    = "modules/network"
   namespace = local.namespace
   region    = local.region
 }
@@ -11,7 +11,7 @@ module "network" {
 # aws에서는 -를 이름에 사용하기 때문에 규칙을 지키기 위해서 다양한 방법 활용 가능
 # terraform 문법에서는 - 가 연산자 취급이 되어 사용 불가능
 module "platform" {
-  source    = "./infra/modules/platform"
+  source    = "modules/platform"
   namespace = local.namespace
   region    = local.region
   vpc_id    = module.network.vpc["main"].id
@@ -26,7 +26,7 @@ module "platform" {
 
 
 module "workload" {
-  source = "./infra/modules/workload"
+  source = "modules/workload"
 
   namespace = local.namespace
   region    = local.region
