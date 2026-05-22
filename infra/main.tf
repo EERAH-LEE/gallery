fe3fdd58c40dd2b5a13e7b0d55a577105db8c8f8
EERAH-LEE <kor.si1229@gmail.com>
2026-05-22 14:42:21 +0900

    test15

--- infra/main.tf
+++ infra/main.tf
@@ -1,54 +1,47 @@
 module "network" {
-  source    = "./modules/network"
+  source = "./modules/network"
   namespace = local.namespace
-
 }
 
-# main에서 불러올 때 '-'는 마이너스 연산자로 들어가기 때문에 - 를 사용할 수 없다.
-# 그래서 그럴 경우에는 key값을 인덱스로 불러오는 것으로 활용하면 됨
-# module.network.subnet.pub-1 == module.network.subnet["pub-1"]
-
-# aws에서는 -를 이름에 사용하기 때문에 규칙을 지키기 위해서 다양한 방법 활용 가능
-# terraform 문법에서는 - 가 연산자 취급이 되어 사용 불가능
 module "platform" {
-  source    = "./modules/platform"
+  source = "./modules/platform"
   namespace = local.namespace
-  vpc_id    = module.network.vpc["main"].id
-  lb_subnets = [
-    # lb라서 pub만 필요
-    module.network.subnet["pub-1"].id,
-    module.network.subnet["pub-2"].id
-  ]
-    lb_target_group_port = local.infra.lb.target_group_port
-    lb_listener_port = local.infra.lb.listener_port
-}
 
+  vpc_id = module.network.vpc["main"].id       #모듈.네트워크.vpc.id 받아옴...(저기 outputs 가서 보면 있는지 확인.)
+  lb_subnets = [
+    module.network.subnet["public-a"].id, 
+    module.network.subnet["public-b"].id
+    ]
+  lb_target_group_port = local.infra.lt.service_port
+  lb_listener_port = local.infra.lb.listener_port
+}
 
 module "workload" {
   source = "./modules/workload"
-
   namespace = local.namespace
-  region    = local.region
+  vpc_id = module.network.vpc["main"].id  
 
-  vpc_id = module.network.vpc["main"].id
   asg_vpc_zone_identifier = [
-    module.network.subnet["pri-1"].id,
-    module.network.subnet["pri-2"].id
-  ]
+    module.network.subnet["private-a"].id, 
+    module.network.subnet["private-b"].id
+    ]
+
   asg_target_group_arns = [module.platform.lb["main"].target_group.arn]
-
-  lt_iam_instance_profile_name = module.platform.iamprofile["main"].name
-
-  lt_allow_access_cidr_blocks = [
-    module.network.subnet["pub-1"].cidr_block,
-    module.network.subnet["pub-2"].cidr_block
-  ]
-  lt_service_port = local.infra.lt.service_port
+  
+  asg_deploy_version = local.infra.asg.deploy_version
 
   asg_min_size = local.infra.asg.min_size
   asg_max_size = local.infra.asg.max_size
   asg_desired_capacity = local.infra.asg.desired_capacity
 
-  asg_deploy_version = local.infra.asg.deploy_version
-  instance_type = local.infra.lt.instance_type
+  lt_iam_instance_profile_name = module.platform.iamprofile["instance"].name
+
+  lt_allow_access_cidr_blocks = [
+    module.network.subnet["public-a"].cidr_blocks, 
+    module.network.subnet["public-b"].cidr_blocks
+    ]
+
+  lt_service_port = local.infra.lt.service_port
+
+  lt_instance_type = local.infra.lt.instance_type
 }
--- infra/modules/network/subnet.tf
+++ infra/modules/network/subnet.tf
@@ -1,12 +1,13 @@
+
 # public-a
 resource "aws_subnet" "public_0" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = local.public_subnet[0].cidr_block
-  availability_zone       = local.public_subnet[0].availability_zone
+  vpc_id = aws_vpc.this.id
+  cidr_block = local.public_subnet[0].cidr_block
+  availability_zone = local.public_subnet[0].availability_zone
   map_public_ip_on_launch = local.public_subnet[0].map_public_ip_on_launch
 
   tags = {
-    Name = "${local.namespace}-subnet-${local.public_subnet[0].name}"
+    Name = "${var.namespace}-subnet-${local.public_subnet[0].name}"
   }
 }
 
@@ -23,20 +24,23 @@ resource "aws_route_table" "public_0" {
   }
 }
 
+# 이 서브넷이 어떤 route table을 사용할지 정하는 부분
 resource "aws_route_table_association" "public_0" {
   subnet_id = aws_subnet.public_0.id
   route_table_id = aws_route_table.public_0.id
 }
 
+
+
 # public-b
 resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = local.public_subnet[1].cidr_block
-  availability_zone       = local.public_subnet[1].availability_zone
+  vpc_id = aws_vpc.this.id
+  cidr_block = local.public_subnet[1].cidr_block
+  availability_zone = local.public_subnet[1].availability_zone
   map_public_ip_on_launch = local.public_subnet[1].map_public_ip_on_launch
 
   tags = {
-    Name = "${local.namespace}-subnet-${local.public_subnet[1].name}"
+    Name = "${var.namespace}-subnet-${local.public_subnet[1].name}"
   }
 }
 
@@ -58,15 +62,17 @@ resource "aws_route_table_association" "public_1" {
   route_table_id = aws_route_table.public_1.id
 }
 
+
+
 # private-c
 resource "aws_subnet" "private_0" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = local.private_subnet[0].cidr_block
-  availability_zone       = local.private_subnet[0].availability_zone
+  vpc_id = aws_vpc.this.id
+  cidr_block = local.private_subnet[0].cidr_block
+  availability_zone = local.private_subnet[0].availability_zone
   map_public_ip_on_launch = local.private_subnet[0].map_public_ip_on_launch
 
   tags = {
-    Name = "${local.namespace}-subnet-${local.private_subnet[0].name}"
+    Name = "${var.namespace}-subnet-${local.private_subnet[0].name}"
   }
 }
 
@@ -88,15 +94,17 @@ resource "aws_route_table_association" "private_0" {
   route_table_id = aws_route_table.private_0.id
 }
 
+
+
 # private-d
 resource "aws_subnet" "private_1" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = local.private_subnet[1].cidr_block
-  availability_zone       = local.private_subnet[1].availability_zone
+  vpc_id = aws_vpc.this.id
+  cidr_block = local.private_subnet[1].cidr_block
+  availability_zone = local.private_subnet[1].availability_zone
   map_public_ip_on_launch = local.private_subnet[1].map_public_ip_on_launch
 
   tags = {
-    Name = "${local.namespace}-subnet-${local.private_subnet[1].name}"
+    Name = "${var.namespace}-subnet-${local.private_subnet[1].name}"
   }
 }
 
