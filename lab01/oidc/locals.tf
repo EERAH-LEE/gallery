3af78eaa28ff880e681236df700a4ebddde4e84d
k0-zzang <hyuna9582@gmail.com>
2026-05-22 09:36:33 +0900

    feat: lab01추가2

--- lab01/oidc/locals.tf
+++ lab01/oidc/locals.tf
@@ -3,7 +3,7 @@ locals {
   project   = "lab01"
   namespace = "${local.org}-${local.project}"
 
-  github_repo = "k0-zzang/gallery-k0"
+  github_repo = "bespin-multi-cloud-3-azure/gallery"
 
   iamrole = {
     name       = "pky"
